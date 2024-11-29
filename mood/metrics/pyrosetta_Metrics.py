f61ec0131cd818d0ff29f1f11766418ec12b0580
AlbertCS <albertcaso22@gmail.com>
2024-11-29 12:05:02 +0100

    deleting the pdb files in the pyrosetta metric not the script

--- mood/metrics/pyrosetta_Metrics.py
+++ mood/metrics/pyrosetta_Metrics.py
@@ -142,9 +142,10 @@ class RosettaMetrics(Metric):
             raise Exception(f"An error occurred while running the Rosetta metrics: {e}")
 
         # Delete the pdb files
-        
-        
-        
+        for file in os.listdir(output_folder):
+            if file.endswith(".pdb"):
+                os.remove(os.path.join(output_folder, file))
+
         if not os.path.exists(f"{output_folder}/rosetta_scores.csv"):
             raise ValueError("The Rosetta metrics did not run successfully")
         else:
